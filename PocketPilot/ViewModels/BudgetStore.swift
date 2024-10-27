ea33e7b3e738c09153e1a2143c8676c4b444f470
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-10-27 17:30:48 +0900

    Implemented a BudgetStore to handling the CRUD logic for CoreData.

--- PocketPilot/ViewModels/BudgetStore.swift
+++ PocketPilot/ViewModels/BudgetStore.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import CoreData
+
+class BudgetStore: ObservableObject {
+  private var viewContext: NSManagedObjectContext
+  
+  @Published var errorMessage: BudgetError? = nil
+  
+  
+  init (content: NSManagedObjectContext) {
+    self.viewContext = content
+  }
+  
+  
+  func exists(context: NSManagedObjectContext, title: String) -> Bool {
+    let request = Budget.fetchRequest()
+    request.predicate = NSPredicate(format: "title == %@", title)
+    
+    do {
+     let results = try context.fetch(request)
+      return !results.isEmpty
+    } catch {
+      return false
+    }
+  }
+  
+  
+  func saveBudget(title: String, limit: Double?)  {
+    let budget = Budget(context: viewContext)
+    budget.title = title
+    budget.limit = limit ?? 0.0
+    budget.dateCreated = Date()
+    do {
+      try viewContext.save()
+      errorMessage = nil
+    } catch {
+      errorMessage = .unableToSave
+    }
+  }
+}
