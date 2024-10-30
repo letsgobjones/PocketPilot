f1a37f404b37302810addd7de5f45fc17f252b1a
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-10-30 11:42:25 +0900

    Created BudgetDetailScreen

---
+++ PocketPilot/Screens/BudgetDetailScreen.swift
@@ -0,0 +1,57 @@
+//
+//  BudgetDetailScreen.swift
+//  PocketPilot
+//
+//  Created by Brandon Jones on 10/30/24.
+//
+
+import SwiftUI
+
+struct BudgetDetailScreen: View {
+  @FetchRequest(sortDescriptors: []) private var budgets: FetchedResults<Budget>
+  
+  let budget: Budget
+  
+  @State private var title: String = ""
+  @State private var amount: Double?
+  
+  private var isFormValid: Bool {
+    !title.isEmptyOrWhitespace && amount != nil && Double(amount!) > 0
+  }
+  
+  
+  var body: some View {
+    Form {
+      
+      Section("New Expense") {
+        TextField("Title", text: $title)
+        TextField("Amount", value: $amount, format: .number)
+          .keyboardType(.numberPad)
+        
+        Button {
+//add func here
+        } label: {
+          Text("Save")
+            .frame(maxWidth: .infinity)
+        }.buttonStyle(.borderedProminent)
+          .disabled(!isFormValid)
+      }
+    }.navigationTitle(budget.title ?? "")
+  }
+}
+
+
+#Preview {
+  
+  var preview: Budget {
+    let budget = Budget(context: CoreDataProvider.preview.context)
+    budget.title = "Sample Budget"
+    budget.limit = 500.00
+    return budget
+  }
+  
+  NavigationStack {
+    BudgetDetailScreen(budget: preview)
+      .environment(\.managedObjectContext, CoreDataProvider.preview.context)
+  }
+}
