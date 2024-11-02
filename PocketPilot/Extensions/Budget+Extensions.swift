9d22f62752dc0debc25e0b4440538ae3a67a43e3
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-11-02 16:14:36 +0900

    Changed milk to expense, for a sample expense.

--- PocketPilot/Extensions/Budget+Extensions.swift
+++ PocketPilot/Extensions/Budget+Extensions.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 extension Budget { 
-    static var preview: Budget {
-        let context = CoreDataProvider.preview.context
-        let budget = Budget(context: context)
-        budget.title = "Sample Budget"
-        budget.limit = 500.00
-
-        let milk = Expense(context: context)
-        milk.title = "Sample Expense"
-        milk.amount = 10.50
-        milk.dateCreated = Date()
-
-        budget.addToExpenses(milk)
-        return budget
-    }
+  static var preview: Budget {
+    let context = CoreDataProvider.preview.context
+    let budget = Budget(context: context)
+    budget.title = "Sample Budget"
+    budget.limit = 500.00
+    
+    let expense = Expense(context: context)
+    expense.title = "Sample Expense"
+    expense.amount = 10.50
+    expense.dateCreated = Date()
+    
+    budget.addToExpenses(expense)
+    return budget
+  }
 }
