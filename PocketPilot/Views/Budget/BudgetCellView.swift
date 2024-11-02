8f02dd5c5bdf0bd3f15aed87eb3e0c54c1b997ca
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-11-02 16:23:58 +0900

    Refactored BudgetCellView

---
+++ PocketPilot/Views/Budget/BudgetCellView.swift
@@ -0,0 +1,38 @@
+//
+//  BudgetCellView.swift
+//  PocketPilot
+//
+//  Created by Brandon Jones on 10/27/24.
+//
+
+
+
+import SwiftUI
+
+
+struct BudgetCellView: View {
+  @EnvironmentObject var budgetStore: BudgetStore
+
+  let budget: Budget
+  
+  var body: some View {
+    HStack {
+      Text(budget.title ?? "")
+      Spacer()
+//      Text(budget.limit, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+      Text(budget.limit, format: .currency(code: budgetStore.selectedCurrency))
+
+    }
+  }
+}
+
+#Preview {
+  NavigationStack {
+    BudgetCellView(budget: Budget.preview)
+      .padding()
+  }
+  .environmentObject(BudgetStore())
+  .environment(\.managedObjectContext, CoreDataProvider.preview.context)
+
+
+}
