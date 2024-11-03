12062d02d0b0494efd285bff70ad2afc62ff593a
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-11-03 14:25:13 +0900

    Updated PocketPilotModel to include Tag entities.

--- PocketPilot/Views/Other/CurrencyPickerView.swift
+++ PocketPilot/Views/Other/CurrencyPickerView.swift
@@ -11,14 +11,13 @@ struct CurrencyPickerView: View {
   
   @EnvironmentObject var budgetStore: BudgetStore
   
-    var body: some View {
-      Picker("Currency", selection: $budgetStore.selectedCurrency) {
-                      Text("USD").tag("USD")
-                      Text("KRW").tag("KRW")
-                  }
-                  .pickerStyle(SegmentedPickerStyle())
-      
+  var body: some View {
+    Picker("Currency", selection: $budgetStore.selectedCurrency) {
+      Text("USD").tag("USD")
+      Text("KRW").tag("KRW")
     }
+    .pickerStyle(SegmentedPickerStyle())
+  }
 }
 
 #Preview {
