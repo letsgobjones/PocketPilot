e90567637196564d61d9ae04131c9739db92eb54
Brandon <89342970+letsgobjones@users.noreply.github.com>
2025-03-30 10:56:40 +0900

    Created EditExpenseScreen.

--- PocketPilot/Screens/EditExpenseScreen.swift
+++ PocketPilot/Screens/EditExpenseScreen.swift
@@ -13,18 +13,50 @@ struct EditExpenseScreen: View {
     sortDescriptors: [NSSortDescriptor(keyPath: \Expense.dateCreated, ascending: false)]
   ) private var expenses: FetchedResults<Expense>
   
+  @Bindable var budgetStore: BudgetStore
   @Environment(\.managedObjectContext) private var viewContext
-  
-  
   let expense: Expense
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  
+  @State private var expenseTitle: String = ""
+  @State private var expenseAmount: Double?
+  @State private var expenseQuantity: Int = 0
+  @State private var expenseSelectedTags: Set<Tag> = []
+  
+  var body: some View {
+    NavigationStack {
+      Form {
+        TextField("Title", text: $expenseTitle)
+        TextField("Amount", value: $expenseAmount, format: .number)
+        TextField("Quantity", value: $expenseQuantity, format: .number)
+        TagsView(selectedTags: $expenseSelectedTags)
+      }
+      .onAppear{
+        expenseTitle = expense.title ?? ""
+        expenseAmount = expense.amount
+        expenseQuantity = Int(expense.quantity)
+        if let tags = expense.tags {
+          expenseSelectedTags = Set(tags.compactMap{$0 as? Tag})
+        }
+      }
+      
+      
+      
+      
+     
+    }.toolbar(content: {
+      ToolbarItem(placement: .topBarTrailing) {
+        Button("Update") {
+          budgetStore.updateExpense(expense: expense, title: expenseTitle, amount: expenseAmount, quantity: expenseQuantity, tags: expenseSelectedTags, context: viewContext)
+        }
+      }
+    })
+    .navigationTitle(expense.title ?? "")
+  }
 }
 
 #Preview {
   NavigationStack {
-    EditExpenseScreen(expense: Expense.preview)
+    EditExpenseScreen(budgetStore: BudgetStore(), expense: Expense.preview)
     
   }
 //  .environment(BudgetStore())
